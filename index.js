e8041a729a94a83336e4f03fd405eea1c942deae
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-05-10 11:19:39 +0200

    test

--- index.js
+++ index.js
@@ -140,10 +140,10 @@ app.get('/db/search', async function(req, res) {
 /* ========================== */
 
 app.get('/api_utente', async function(req, res) {
-	/*res.send(await template.generate('generic.html', {
-		text: "Hello world as a Handlebar service",
-	}));*/
-	res.send(await mymongo.search_utente(req.query, mongoCredentials))
+	res.send(await template.generate('generic.html', {
+		text: req,
+	}));
+	//res.send(await mymongo.search_utente(req.query, mongoCredentials))
 });
 
 /* ========================== */
