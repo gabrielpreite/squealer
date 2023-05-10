8f9aecd94cd315a2732227b2303b77d60612bdfb
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-05-10 11:21:44 +0200

    nth test

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ app.get('/db/search', async function(req, res) {
 
 app.get('/api_utente', async function(req, res) {
 	res.send(await template.generate('generic.html', {
-		text: req,
+		text: req.query,
 	}));
 	//res.send(await mymongo.search_utente(req.query, mongoCredentials))
 });
