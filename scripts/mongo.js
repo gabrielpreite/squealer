d478a97192a10053de9d54e2974b67bad7c5a65b
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-05-10 11:56:11 +0200

    test

--- scripts/mongo.js
+++ scripts/mongo.js
@@ -107,7 +107,7 @@ exports.create = async function(credentials) {
 }
 
 exports.search_utente = async function(q,credentials) {
-	return "ciao"
+	return JSON.stringify(q)
 	/*const mongouri = `mongodb://${credentials.user}:${credentials.pwd}@${credentials.site}?writeConcern=majority`;
 
 	let query =  {}
