5612685b878912006f24b31ae6d65da980296d36
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-06-09 10:28:07 +0200

    getCookie

---
+++ scripts/global.js
@@ -0,0 +1,11 @@
+//funzioni globali
+
+function check_cookie_name(name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) {
+    console.log(match[2]);
+    }
+    else{
+        console.log('--something went wrong---');
+    }
+}
