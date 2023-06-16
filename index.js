847effda0c6e8d953292ecc394551388a84cc705
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-06-16 09:44:02 +0200

    test

--- index.js
+++ index.js
@@ -222,14 +222,16 @@ app.get('/permessi_canale', async function(req, res) {
 	try{
 
 		let result = await mymongo.permessi_canale(req.query, session.userid, mongoCredentials)
-		if(result[0]["abilitato"] == true){
+		/*if(result[0]["abilitato"] == true){
 			if(result[0]["scrittura"].includes(session.userid) || result[0]["scrittura"][0] == "*"){
 				res.send("{result: 'true'}")
 			}
 		}
-		res.send("{result: 'false'}")
+		res.send("{result: 'false'}")*/
+		res.send(JSON.stringify(result))
 	}catch(e){
-		res.send(`{result: '${JSON.stringify(req)}'}`)
+		//res.send(`{result: '${JSON.stringify(req)}'}`)
+		res.send(e)
 	}
 });
 
