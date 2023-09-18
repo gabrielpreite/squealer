cc1e73d8f053ea11611b598a1f3ec5611e841755
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-09-18 16:48:08 +0200

    test redirect replace frontend

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ app.post('/crea_post', async function(req, res) {
 	}
 	res.status(200)
 	//res.send("ok")
-	res.redirect("/")
+	//res.redirect("/")
 });
 
 //risultati della ricerca tramite searchbar
