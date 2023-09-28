ca86bc0ee615e0fbb1cf62d689b905e50c77bb64
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-09-28 18:04:16 +0200

    test path

--- multer.js
+++ multer.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/media/uploads/'); // Destination folder for uploaded files
+    cb(null, './'); // Destination folder for uploaded files
   },
   filename: (req, file, cb) => {
     const fileName = Date.now() + '-' + file.originalname;
