990dccd6b07bac6b6fe55d783c532230e040ee7a
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-09-29 15:11:20 +0200

    test rootdir

--- multer.js
+++ multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, '../public/media/uploads'); // Construct the path
+    const uploadPath = path.join(__dirname, '/public/media/uploads'); // Construct the path
     cb(null, uploadPath); // Use the constructed path
   },
   filename: (req, file, cb) => {
