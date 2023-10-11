ece986a450ed6051aacb91abe1fc72a2dadec732
near237 <s.giannone23@gmail.com>
2023-10-11 17:02:00 +0200

    cambiati path img

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ﻿/*
 File: index.js
 Author: Fabio Vitali
-Version: 1.0 
+Version: 1.0
 Last change on: 10 April 2021
 
 
@@ -30,10 +30,10 @@ Copyright (c) 2021 by Fabio Vitali
 /* ========================== */
 
 global.rootDir = __dirname ;
-global.startDate = null; 
+global.startDate = null;
 
-const template = require(global.rootDir + '/scripts/tpl.js') ; 
-const mymongo = require(global.rootDir + '/scripts/mongo.js') ; 
+const template = require(global.rootDir + '/scripts/tpl.js') ;
+const mymongo = require(global.rootDir + '/scripts/mongo.js') ;
 const express = require('express') ;
 const cors = require('cors')
 const cookieParser = require("cookie-parser");
@@ -52,16 +52,17 @@ const upload = require('./multer'); // Import the multer configuration
 /*                            */
 /* ========================== */
 
-let app= express(); 
+let app= express();
 app.use('/js'  , express.static(global.rootDir +'/public/js'));
 app.use('/css' , express.static(global.rootDir +'/public/css'));
 app.use('/data', express.static(global.rootDir +'/public/data'));
 app.use('/docs', express.static(global.rootDir +'/public/html'));
-app.use('/img' , express.static(global.rootDir +'/public/media'));
+app.use('/media' , express.static(global.rootDir +'/public/media'));
+app.use('/img' , express.static(global.rootDir +'/public/media/img'));
 app.use('/uploads', express.static(global.rootDir +'/public/media/uploads'));
 app.use(express.json()); //?
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 var session;
 const oneDay = 1000 * 60 * 60 * 24;
@@ -69,14 +70,14 @@ app.use(sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
     saveUninitialized:true,
     cookie: { maxAge: oneDay },
-    resave: false 
+    resave: false
 }));
 
 // https://stackoverflow.com/questions/40459511/in-express-js-req-protocol-is-not-picking-up-https-for-my-secure-link-it-alwa
 app.enable('trust proxy');
 
 
-/*app.get('/', async function (req, res) { 
+/*app.get('/', async function (req, res) {
 	let sitename = req.hostname.split('.')[0]
 	res.send(await template.generate('index.html', {
 			host: req.hostname,
@@ -84,17 +85,17 @@ app.enable('trust proxy');
 	}));
 })*/
 
-app.get('/', function (req, res) { 
+app.get('/', function (req, res) {
 	if(!req.session || !req.session.userid) {res.redirect("/login")}
 	res.sendFile(global.rootDir+"/public/html/feed.html")
 })
 
-app.get('/editor', function (req, res) { 
+app.get('/editor', function (req, res) {
 	if(!req.session || !req.session.userid) {res.redirect("/login")}
 	res.sendFile(global.rootDir+"/public/html/editor.html")
 })
 
-app.get('/settings', function (req, res) { 
+app.get('/settings', function (req, res) {
 	if(!req.session || !req.session.userid) {res.redirect("/login")}
 	res.sendFile(global.rootDir+"/public/html/settings.html")
 })
@@ -104,20 +105,20 @@ app.get('/testdb', function (req, res) { // ---- DEBUG USE
 	res.sendFile(global.rootDir+"/public/html/testdb.html")
 })
 
-app.get('/login', function (req, res) { 
+app.get('/login', function (req, res) {
 	res.sendFile(global.rootDir+"/public/html/login.html")
 })
 
-app.get('/logout', function (req, res) { 
+app.get('/logout', function (req, res) {
 	req.session.destroy()
 	res.redirect("/login")
 })
 
-app.get('/test', function (req, res) { 
+app.get('/test', function (req, res) {
 	res.sendFile(global.rootDir+"/public/html/test.html")
 })
 
-/*app.get('/hw', async function(req, res) { 
+/*app.get('/hw', async function(req, res) {
 	var text = "Hello world as a Node service";
 	res.send(
 `<!doctype html>
@@ -130,7 +131,7 @@ app.get('/test', function (req, res) {
 			`)
 });
 
-app.get('/hwhb', async function(req, res) { 
+app.get('/hwhb', async function(req, res) {
 	res.send(await template.generate('generic.html', {
 		text: "Hello world as a Handlebar service",
 	}));
@@ -138,14 +139,14 @@ app.get('/hwhb', async function(req, res) {
 
 const info = async function(req, res) {
 	let data = {
-		startDate: global.startDate.toLocaleString(), 
-		requestDate: (new Date()).toLocaleString(), 
+		startDate: global.startDate.toLocaleString(),
+		requestDate: (new Date()).toLocaleString(),
 		request: {
 			host: req.hostname,
 			method: req.method,
 			path: req.path,
 			protocol: req.protocol
-		}, 
+		},
 		query: req.query,
 		body: req.body
 	}
@@ -165,18 +166,18 @@ app.post('/info', info )
 /*                            */
 /* ========================== */
 
-/* Replace these info with the ones you were given when activating mongoDB */ 
+/* Replace these info with the ones you were given when activating mongoDB */
 const mongoCredentials = {
         user: "site212251",
         pwd: "iot7Eewe",
         site: "mongo_site212251"
-}  
+}
 /* end */
 
-app.get('/db/create', async function(req, res) { 
+app.get('/db/create', async function(req, res) {
 	res.send(await mymongo.create(mongoCredentials))
 });
-app.get('/db/search', async function(req, res) { 
+app.get('/db/search', async function(req, res) {
 	res.send(await mymongo.search(req.query, mongoCredentials))
 });
 
@@ -325,10 +326,10 @@ app.post('/upload', upload.single('img'), (req, res) => {  // ---- DEBUG USE
 	if (!req.file) {
 	  return res.status(400).json({ message: 'No file uploaded' });
 	}
-  
+
 	// You can perform further processing here, such as saving the file path to a database
 	const imagePath = req.file.path;
-  
+
 	return res.status(200).json({ message: 'File uploaded successfully', imagePath });
   });
 
@@ -352,8 +353,8 @@ app.use(function(req, res){
 /*                            */
 /* ========================== */
 
-app.listen(8000, function() { 
-	global.startDate = new Date() ; 
+app.listen(8000, function() {
+	global.startDate = new Date() ;
 	console.log(`App listening on port 8000 started ${global.startDate.toLocaleString()}` )
 })
 
