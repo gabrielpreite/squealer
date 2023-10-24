5f8d33d70f5e4d2abd30e36b3e84ca330de3bfde
Matteo99591 <matteoleccabue@gmail.com>
2023-10-24 16:30:21 +0200

    timestamp

--- public/js/global.js
+++ public/js/global.js
@@ -34,3 +34,15 @@ function redirectToSettings() {
 function redirectToHome() {
     window.location.href = "/";
 }
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var year = a.getFullYear();
+    var month = a.getMonth();
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + '/' + month + '/' + year + ' ' + hour + ':' + min;
+    return time;
+}
