aad2bbb8f01e6a2ea1f08a489172314386af8335
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-10-30 17:50:12 +0100

    debug

--- scripts/mongo.js
+++ scripts/mongo.js
@@ -176,7 +176,8 @@ exports.search_messaggio = async function(q,credentials) {
 			debug.push("found args :"+q.messaggio_id)
 			await mongo.db(dbname) // TODO nome ai post, regole di visibilita', ordine
 				.collection("messaggio")
-				.aggregate([
+				.find({_id: q.messaggio_id})
+				/*.aggregate([
 					{ $match: { _id: q.messaggio_id } },
 					{ $lookup: {
 						from: "utente", // nome seconda tabella
@@ -203,7 +204,7 @@ exports.search_messaggio = async function(q,credentials) {
 					} },
 					{ $addFields: { numRisposte: { $size: "$risposte" } } },
 					{ $project: { risposte: 0 } }
-				  ])
+				  ])*/
 				.forEach( (r) => { 
 					result.push(r) 
 				});
