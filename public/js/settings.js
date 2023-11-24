45f80f8e11666ccdeb1a2154164aa99b321e65c3
Matteo99591 <matteoleccabue@gmail.com>
2023-11-24 18:06:15 +0100

    aa

--- public/js/settings.js
+++ public/js/settings.js
@@ -98,7 +98,7 @@ function check_form() {
     if (document.getElementById("newEmail") != '') {
         //check formato mail corretto (RFC 2822 standard email validation)
         var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-        if (!document.getElementById("email").value.match(mailformat)) {
+        if (!document.getElementById("newEmail").value.match(mailformat)) {
             alert("La nuova mail inserita non è valida");
             return false;
         }
