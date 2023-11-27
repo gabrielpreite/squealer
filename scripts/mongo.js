3a2d6e11c971972a1fe1b6827ff717fcd524ff9a
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-11-27 16:45:17 +0100

    crea chat

--- scripts/mongo.js
+++ scripts/mongo.js
@@ -647,12 +647,26 @@ exports.post_chat = async function(target, q, credentials) {
 
         if(result.matchedCount == 1){
             response["risultato"] = "successo"
-			// todo - notifica dm
 			//console.log("successo")
-        } else {
-            response["risultato"] = "chat non trovata"
-			//console.log("errati")
+        } else { //creo la chat
+            await mongo.db(dbname)
+				.collection("chat")
+				.insertOne(
+					{
+						"chat_id": id,
+						"users": [origin, target],
+						"messaggi": [
+							{
+								"text": val,
+								"user": origin,
+								"timestamp": timestamp
+							}
+						]
+					}
+				)
         }
+
+		// todo - notifica dm
 
 		response["data"] = result[0]
         await mongo.close();
