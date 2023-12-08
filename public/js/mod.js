c9e191a977e623a583b6f57996f61950683c24d3
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-12-08 17:40:30 +0100

    selected user

--- public/js/mod.js
+++ public/js/mod.js
@@ -14,13 +14,36 @@ function cat_utenti(){
     
     document.getElementById("user_table").innerHTML = ""
     let tableBody = document.createElement("tbody");
+    tableBody.insertAdjacentHTML("afterbegin", "<tr><th>Username</th><th>Redazione</th><th>Verificato</th><th>Professional</th><th>Popolarita</th></tr>")
     utenti.forEach((user) => {
         var row = document.createElement("tr");
+
         let cell = document.createElement("td");
         let cellText = document.createTextNode(user.username);
-
         cell.appendChild(cellText);
         row.appendChild(cell);
+
+        let cell_red = document.createElement("td");
+        let cellText_red = document.createTextNode(user.redazione_flag ? "Y" : "N");
+        cell_red.appendChild(cellText_red)
+        row.appendChild(cell_red);
+
+        let cell_ver = document.createElement("td");
+        let cellText_ver = document.createTextNode(user.verificato_flag ? "Y" : "N");
+        cell_ver.appendChild(cellText_ver)
+        row.appendChild(cell_ver);
+
+        let cell_pro = document.createElement("td");
+        let cellText_pro = document.createTextNode(user.professional_flag ? "Y" : "N");
+        cell_pro.appendChild(cellText_pro)
+        row.appendChild(cell_pro);
+
+        let cell_pop = document.createElement("td");
+        let cellText_pop = document.createTextNode(user.popolarita.valori[user.popolarita.valori.length-1]);
+        cell_pop.appendChild(cellText_pop)
+        row.appendChild(cell_pop);
+
+        row.onclick = function() { seleziona_utente(user.username); };
         tableBody.appendChild(row)
     })
     document.getElementById("user_table").appendChild(tableBody)
@@ -31,9 +54,9 @@ function cat_utenti(){
 }
 
 function compare_utenti(a, b){
-    if(a.popolarita.valori[a.popolarita.valori.length] < b.popolarita.valori[b.popolarita.valori.length]){
+    if(a.popolarita.valori[a.popolarita.valori.length-1] < b.popolarita.valori[b.popolarita.valori.length-1]){
         return -1
-    } else if(a.popolarita.valori[a.popolarita.valori.length] > b.popolarita.valori[b.popolarita.valori.length]){
+    } else if(a.popolarita.valori[a.popolarita.valori.length-1] > b.popolarita.valori[b.popolarita.valori.length-1]){
         return 1
     } else {
         return 0
@@ -44,7 +67,7 @@ function sort_utenti(){
     let tipo = document.getElementById("user_filtro_tipo").value
     let query
     let new_utenti = []
-    
+
     if(tipo === "nome"){ //solo quelli che matchano sul nome
         query = document.getElementById("user_query_nome").value
         utenti.forEach((el) => {
@@ -69,13 +92,36 @@ function sort_utenti(){
 
     document.getElementById("user_table").innerHTML = ""
     let tableBody = document.createElement("tbody");
+    tableBody.insertAdjacentHTML("afterbegin", "<tr><th>Username</th><th>Redazione</th><th>Verificato</th><th>Professional</th><th>Popolarita</th></tr>")
     new_utenti.forEach((user) => {
         var row = document.createElement("tr");
+
         let cell = document.createElement("td");
         let cellText = document.createTextNode(user.username);
-
         cell.appendChild(cellText);
         row.appendChild(cell);
+
+        let cell_red = document.createElement("td");
+        let cellText_red = document.createTextNode(user.redazione_flag ? "Y" : "N");
+        cell_red.appendChild(cellText_red)
+        row.appendChild(cell_red);
+
+        let cell_ver = document.createElement("td");
+        let cellText_ver = document.createTextNode(user.verificato_flag ? "Y" : "N");
+        cell_ver.appendChild(cellText_ver)
+        row.appendChild(cell_ver);
+
+        let cell_pro = document.createElement("td");
+        let cellText_pro = document.createTextNode(user.professional_flag ? "Y" : "N");
+        cell_pro.appendChild(cellText_pro)
+        row.appendChild(cell_pro);
+        
+        let cell_pop = document.createElement("td");
+        let cellText_pop = document.createTextNode(user.popolarita.valori[user.popolarita.valori.length-1]);
+        cell_pop.appendChild(cellText_pop)
+        row.appendChild(cell_pop);
+        
+        row.onclick = function() { seleziona_utente(user.username); };
         tableBody.appendChild(row)
     })
     document.getElementById("user_table").appendChild(tableBody)
@@ -92,4 +138,32 @@ function cambia_query_user(){
     if(scelta === "nome"){
         document.getElementById("user_query_nome").classList.remove("hidden-text-input")
     }
+}
+
+function seleziona_utente(username){
+    let user
+    $.ajax({
+        type: 'GET',
+        dataType: "json",
+        async: false,
+        url: `https://site212251.tw.cs.unibo.it/user/`+username,
+        headers: { },
+        success: function (data, status, xhr) {
+          user = data.data;
+          console.log(data)
+          document.getElementById("right_user").removeAttribute("hidden")
+          document.getElementById("right_squeal").setAttribute("hidden", "")
+          document.getElementById("right_canali").setAttribute("hidden", "")
+      
+          document.getElementById("selected_user_username").innerHTML = user.username
+          document.getElementById("abilitato_user_switch").checked = user.abilitato_flag
+          document.getElementById("quota_g").value = user.quota.g
+          document.getElementById("quota_s").value = user.quota.s
+          document.getElementById("quota_m").value = user.quota.m
+        }
+    });
+}
+
+function applica_utenti(){
+
 }
