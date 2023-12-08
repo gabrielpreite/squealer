9653ab90d8df145f99cdca66c3b4f1afa273b8a8
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-12-08 16:20:16 +0100

    filtri utente mod

--- public/js/mod.js
+++ public/js/mod.js
@@ -1,5 +1,5 @@
 function cat_utenti(){
-    let utenti = []
+    var utenti = []
     $.ajax({
         type: 'GET',
         dataType: "json",
@@ -10,9 +10,8 @@ function cat_utenti(){
           utenti = data;
         }
     });
-
-    let anchor = document.getElementById("content_mid")
-    let table = document.createElement("table");
+    
+    document.getElementById("user_table").innerHTML = ""
     let tableBody = document.createElement("tbody");
     utenti.forEach((user) => {
         var row = document.createElement("tr");
@@ -23,7 +22,29 @@ function cat_utenti(){
         row.appendChild(cell);
         tableBody.appendChild(row)
     })
+    document.getElementById("user_table").appendChild(tableBody)
+    document.getElementById("mid_user").removeAttribute("hidden")
+    document.getElementById("mid_squeal").setAttribute("hidden", "true")
+    document.getElementById("mid_canali").setAttribute("hidden", "true")
 
-    table.appendChild(tableBody)
-    anchor.appendChild(table)
+}
+
+function sort_utenti(tipo, query){
+    if(tipo === "nome"){ //solo quelli che matchano sul nome
+
+    } else if(tipo === "tipo") { //solo quelli che hanno flag redazione|verificato|professional(_flag)
+
+    } else if(tipo === "popolarita"){ //ordine crescente|decrescente
+
+    }
+
+}
+
+function cambia_query_user(){
+    document.getElementById("user_query_nome").setAttribute("hidden", "true")
+    document.getElementById("user_query_tipo").setAttribute("hidden", "true")
+    document.getElementById("user_query_popolarita").setAttribute("hidden", "true")
+
+    let scelta = document.getElementById("user_filtro_tipo").value
+    document.getElementById("user_query_"+scelta).removeAttribute("hidden")
 }
