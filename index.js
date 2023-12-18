d536bb13c3e2db68cad68bba94483c2c7813e717
Gabriel Preite <gabriel.preite@studio.unibo.it>
2023-12-18 16:00:55 +0100

    fetch

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const { escapeExpression } = require('handlebars');
 const upload = require('./multer');
 const { Timestamp } = require('mongodb');
 const schedule = require('node-schedule');
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
 /* ========================== */
 /*                            */
