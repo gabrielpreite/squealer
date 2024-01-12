9dd887364dc5c33b03ba6d7cb6445b2c006656a6
Matteo99591 <matteoleccabue@gmail.com>
2024-01-12 10:40:07 +0100

    dest try

--- public/js/app-editor.js
+++ public/js/app-editor.js
@@ -31,15 +31,174 @@ function cambia_campo(opzione) {
     }
 }
 
-function resetQuota() {
-    charCount_g.textContent = initialQuota_g;
-    charCount_s.textContent = initialQuota_s;
-    charCount_m.textContent = initialQuota_m;
+//DESTINATARI
+function mostraCampo(tipo) {
+    var destinatariInseritiDiv = document.getElementById("destinatari-inseriti");
 
-    charCount_g.classList.remove('negative');
-    charCount_s.classList.remove('negative');
-    charCount_m.classList.remove('negative');
+    // Nascondi tutti i campi inizialmente
+    document.getElementById("campo-canali").hidden = true;
+    document.getElementById("campo-utenti").hidden = true;
+    document.getElementById("lista_destinatari").hidden = false;
+    document.getElementById("campo-bacheca").hidden = true;
 
-    button.classList.remove('button-disabled');
-}
+    // Rimuovi la classe 'attiva' da tutte le icone
+    var icone = document.querySelectorAll(".tipo-icona");
+    icone.forEach(function (icona) {
+      icona.classList.remove("attiva");
+    });
 
+    // Mostra il campo corrispondente al tipo selezionato
+    if (tipo === "canali") {
+      document.getElementById("campo-canali").hidden = false;
+      document.getElementById("lista_destinatari").hidden = true;
+      destinatariInseritiDiv.innerHTML = "";
+      array_dest_utenti = [];
+      document.getElementById("icona-canali").classList.add("attiva");
+
+    } else if (tipo === "utenti") {
+      document.getElementById("campo-utenti").hidden = false;
+      document.getElementById("lista_destinatari").hidden = true;
+      destinatariInseritiDiv.innerHTML = "";
+      array_dest_canali = [];
+      document.getElementById("icona-utenti").classList.add("attiva");
+
+    } else if (tipo === "bacheca") {
+      document.getElementById("lista_destinatari").hidden = true;
+      document.getElementById("campo-bacheca").hidden = false;
+      destinatariInseritiDiv.innerHTML = "";
+      array_dest_utenti = [];
+      array_dest_canali = [];
+      document.getElementById("icona-bacheca").classList.add("attiva");
+
+    }
+  }
+
+
+  function check_destinatari_canali() {
+    var a = $("#cerca_destinatari_canali").val();//nome destinatario da cercare (per ora gestisco solo canali)
+    if (!a.startsWith('$')) {
+      a = "$".concat(a)
+    }
+    $.ajax({
+      type: 'GET',
+      dataType: "json",
+      url: `${window.location.origin}/channel/${a}/auth?userid=${CURRENT_USER}`,
+      headers: {
+      },
+      success: function (data, status, xhr) {//canale esiste e utente ha i permessi
+
+        //rimuovo $ iniziale per visualizzazione in lista
+        a = a.substring(1)
+        if (!array_dest_canali.includes(a)) {
+            // Aggiungi il destinatario al riquadro dei destinatari inseriti
+            $("#destinatari-inseriti").append(`
+              <div class="inserito-item name-${a}">
+              <div class="left-content">
+                <i class="fas fa-users"></i>
+                <p>${a}</p>
+              </div>
+              <div class="right-content">
+                <p class="rimuovi" onclick="rimuovi_destinatario('${a}', array_dest_canali)"><i class="fas fa-times"></i></p>
+              </div>
+              </div>
+            `);
+            document.getElementById("lista_destinatari").hidden = false;
+            array_dest_canali.push(a); // Aggiungi all'array
+            $("#cerca_destinatari_canali").val(""); // Resetta il campo
+          }
+      },
+      error: function (xhr, status, e){
+        alert("Il canale selezionato non esiste o l'utente non ha permessi di scrittura")
+      }
+    });
+  }
+
+  function check_destinatari_utenti() {
+    var a = $("#cerca_destinatari_utenti").val();//nome utente da cercare
+    if (a.startsWith('@')) {
+      a = a.substring(1)
+    } else if (a == get_cookie_by_name("username")) {
+      alert("Non è possibile inserire il proprio username tra i destinatari");
+      return;
+    }
+    $.ajax({
+      type: 'GET',
+      dataType: "json",
+      url: `${window.location.origin}/user/${a}`,
+      headers: {
+      },
+      success: function (data, status, xhr) {//canale esiste e utente ha i permessi
+        if (!array_dest_utenti.includes(a)) {
+          // Aggiungi il destinatario al riquadro dei destinatari inseriti
+          $("#destinatari-inseriti").append(`
+            <div class="inserito-item name-${a}">
+            <div class="left-content">
+              <i class="fas fa-users"></i>
+              <p>${a}</p>
+            </div>
+            <div class="right-content">
+              <p class="rimuovi" onclick="rimuovi_destinatario('${a}', array_dest_utenti)"><i class="fas fa-times"></i></p>
+              </div>
+            </div>
+          `);
+          document.getElementById("lista_destinatari").hidden = false;
+          array_dest_utenti.push(a); // Aggiungi all'array
+          $("#cerca_destinatari_utenti").val(""); // Resetta il campo
+        }
+      },
+      error: function (xhr, status, e){
+        alert("L'utente selezionato non esiste")
+      }
+    });
+  }
+
+  function rimuovi_destinatario(nome, array_dest) {
+    $(`#destinatari-inseriti .name-${nome}`).remove();
+
+    // Rimuovi il destinatario dall'array
+    const index = array_dest.indexOf(nome);
+    if (index !== -1) {
+      array_dest.splice(index, 1);
+    }
+    var destinatariInseritiDiv = document.getElementById("destinatari-inseriti");
+    var listaDestinatariDiv = document.getElementById("lista_destinatari");
+
+    // Verifica se il div "destinatari-inseriti" è vuoto
+    if (destinatariInseritiDiv.innerHTML.trim() == "") {
+      // Imposta "hidden" su true per "lista_destinatari"
+      listaDestinatariDiv.hidden = true;
+    } else {
+      // Altrimenti, imposta "hidden" su false per "lista_destinatari"
+      listaDestinatariDiv.hidden = false;
+    }
+  }
+
+  function check_post() {
+    // Verifica se ci sono destinatari selezionati
+    let arr = window.location.href.split('?');
+    if (!((arr.length > 1 && arr[1] !== '') || (!document.getElementById("campo-bacheca").hidden))) { //caso risposta o bacheca, no destinatari
+      if ($("#destinatari-inseriti").children().length == 0) {
+        alert("Nessun destinatario selezionato");
+        return false;
+      }
+    }
+    if(!$("#contenuto_testo").attr("hidden")) {//caso testo
+      if (document.getElementById("Textarea").value == '') {
+        alert("Nessun testo inserito");
+        return false;
+      }
+    } else if (!$("#contenuto_immagine").attr("hidden")) {//caso img
+      if (document.getElementById("img").files.length == 0) {
+        alert("Nessuna immagine inserita");
+        return false;
+      }
+    } else if (!$("#contenuto_posizione").attr("hidden")) {//caso map
+      if (marker.position == undefined) {
+        alert("Nessuna posizione inserita");
+        return false;
+      }
+      mapeado(map, marker);
+    }
+    // Altri controlli se necessari, come il testo, immagini o posizione
+    return true;
+  }
