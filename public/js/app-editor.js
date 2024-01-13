a26f6c72c7575350d9a1224514db3b47bd2344ae
Matteo99591 <matteoleccabue@gmail.com>
2024-01-13 11:48:38 +0100

    asd

--- public/js/app-editor.js
+++ public/js/app-editor.js
@@ -214,18 +214,18 @@ function add_post(){
                 array_dest[x] = chip_arr[0].chipsData[x].tag; 
                 x = x + 1;
             });
-            if(!$("#campo-canali").attr("hidden")){//caso canali
-            //aggiungo $ prima di ogni canale
-            array_dest.forEach(function(nome, index, array) {
-                array[index] = "$" + nome;
-            });
-            formData.append("destinatari", JSON.stringify(array_dest))
-            formData.append("tipo_destinatari", "canali")
-            }else if(!$("#campo-utenti").attr("hidden")){//caso utenti
-            formData.append("destinatari", JSON.stringify(array_dest))
-            formData.append("tipo_destinatari", "utenti")
+            if(!$("#icona-canali").attr("hidden")){//caso canali
+                //aggiungo $ prima di ogni canale
+                array_dest.forEach(function(nome, index, array) {
+                    array[index] = "$" + nome;
+                });
+                formData.append("destinatari", JSON.stringify(array_dest))
+                formData.append("tipo_destinatari", "canali")
+            }else if(!$("#icona-utenti").attr("hidden")){//caso utenti
+                formData.append("destinatari", JSON.stringify(array_dest))
+                formData.append("tipo_destinatari", "utenti")
             }else if(!$("#campo-bacheca").attr("hidden")){//caso bacheca
-            formData.append("destinatari", JSON.stringify([]))
+                formData.append("destinatari", JSON.stringify([]))
             }
         }
 
