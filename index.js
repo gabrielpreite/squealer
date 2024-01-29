ff21631864ea42863a58d709b924529bfe2bcdf3
Gabriel Preite <gabriel.preite@studio.unibo.it>
2024-01-29 09:28:32 +0100

    vue config

--- index.js
+++ index.js
@@ -383,7 +383,7 @@ app.get('/db/chat', async function(req, res) {
 
 // dry daily run
 app.get('/db/dry_daily', async function(req, res) {
-	res.send(await mymongo.daily(true, mongoCredentials))
+	res.send(await mymongo.daily(false, mongoCredentials))
 });
 
 // dry weekly run
